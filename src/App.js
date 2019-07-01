49ec8d01141511cb8d82772996e9bb65b8f85e82
Michael Vo <michaelmvo91@gmail.com>
2019-07-01 12:16:22 -1000

    Adding fontawesome library

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import './App.css';
+import {library} from '@fortawesome/fontawesome-svg-core'
+import {faChevronRight} from '@fortawesome/free-solid-svg-icons'
 import Container from "react-bootstrap/Container";
 import {BrowserRouter as Router, Route} from "react-router-dom";
+import Navigation from "./components/Navigation";
 import Login from "./pages/Login"
 import Register from "./pages/Register";
 import Reset from "./pages/Reset"
@@ -10,6 +13,8 @@ import Episodes from "./pages/Episodes"
 import Seasons from "./pages/Seasons";
 import Queue from "./pages/Queue";
 import New from "./pages/New"
+
+library.add(faChevronRight);
 
 function App() {
     return (
