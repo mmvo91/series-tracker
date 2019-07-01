52e73013d266d005d787206923427c6cb3da430c
Michael Vo <michaelmvo91@gmail.com>
2019-07-01 11:22:30 -1000

    Implementing React Router and routes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Container from "react-bootstrap/Container";
+import {BrowserRouter as Router, Route} from "react-router-dom";
+import Login from "./pages/Login"
+import Register from "./pages/Register";
+import Reset from "./pages/Reset"
+import Shows from "./pages/Shows";
+import Episodes from "./pages/Episodes"
+import Seasons from "./pages/Seasons";
+import Queue from "./pages/Queue";
+import New from "./pages/New"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <Container fluid className="py-2">
+                <Route exact path="/" component={Shows}/>
+                <Route path="/login" component={Login}/>
+                <Route path="/register" component={Register}/>
+                <Route path="/reset" component={Reset}/>
+                <Route exact path="/new" component={New}/>
+                <Route exact path="/queue" component={Queue}/>
+                <Route exact path="/shows" component={Shows}/>
+                <Route exact path="/shows/:id" component={Episodes}/>
+                <Route exact path="/shows/:id/season" component={Seasons}/>
+                <Route exact path="/shows/:id/season/:season" component={Episodes}/>
+            </Container>
+        </Router>
+    );
 }
 
 export default App;
