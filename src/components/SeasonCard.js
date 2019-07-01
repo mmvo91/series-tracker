a5a559ea62de34a9c1eb96c490a29d6c7c2b321d
Michael Vo <michaelmvo91@gmail.com>
2019-07-01 11:09:25 -1000

    Adding Season page and component

---
+++ src/components/SeasonCard.js
@@ -0,0 +1,43 @@
+import React from "react";
+import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
+import Image from "react-bootstrap/Image";
+import Button from "react-bootstrap/Button";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+
+const Season = (props) => {
+    return (
+        <Col md={3} className="py-2">
+            <Card>
+                <Card.Body>
+                    <Card.Title>
+                        {
+                            props.name
+                                ? props.name
+                                : "Season " + props.number
+                        }
+                    </Card.Title>
+
+                    <Image fluid src={props.image}/>
+
+                    <ButtonGroup>
+                        <Button href={"/shows/" + props.show_id + "/season/" + props.number}>
+                            Episodes
+                        </Button>
+                        {
+                            props.watched
+                                ? <Button onClick={props.update} key={props.number} id={props.number} variant="danger">Mark
+                                    Not
+                                    Watched</Button>
+                                : <Button onClick={props.update} key={props.number} id={props.number}>Mark
+                                    Watched</Button>
+                        }
+                    </ButtonGroup>
+                </Card.Body>
+            </Card>
+        </Col>
+    )
+
+};
+
+export default Season
---
+++ src/pages/Seasons.js
@@ -0,0 +1,61 @@
+import React, {Component} from "react";
+import {Row} from "react-bootstrap";
+import api from "../Api"
+import Season from "../components/SeasonCard"
+
+export default class Seasons extends Component {
+    state = {
+        data: []
+    };
+
+    componentDidMount() {
+        api.get('/users/1/subscriptions/' + this.props.match.params.id + '/seasons')
+            .then(res => {
+                this.setState({data: res.data});
+            })
+    }
+
+    update = (e) => {
+        e.preventDefault();
+
+        const season = e.target.id;
+
+        let idx = this.state.data.findIndex(item => {
+            return item.season.number.toString() === season.toString()
+        });
+
+        let watched = !this.state.data[idx]['watched'];
+
+        const data = {
+            season: season,
+            watched: watched
+        };
+
+        api.put('/users/1/subscriptions/' + this.props.match.params.id + '/seasons', data)
+            .then(res => {
+                let current_data = this.state.data;
+                current_data[idx]['watched'] = watched;
+                this.setState({
+                    data: current_data
+                })
+            })
+    };
+
+    render() {
+        return (
+            <Row>
+                {
+                    this.state.data.map((datum) =>
+                        (
+                            <Season
+                                show_id={this.props.match.params.id}
+                                key={datum}
+                                update={this.update}
+                                watched={datum.watched}
+                                {...datum.season}/>
+                        ))
+                }
+            </Row>
+        )
+    }
+}
