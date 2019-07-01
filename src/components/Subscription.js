e284f3273cd6cdd109a3ee9a88f18304d7928ff2
Michael Vo <michaelmvo91@gmail.com>
2019-07-01 11:13:40 -1000

    Adding subscription component

---
+++ src/components/Subscription.js
@@ -0,0 +1,58 @@
+import React, {Component} from "react";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import axios from "axios";
+
+export default class Subscription extends Component {
+    state = {
+        show: null,
+        msg: null
+    };
+
+    changeShow = (e) => {
+        this.setState({
+            show: e.target.value
+        })
+    };
+
+    newSub = (x) => {
+        x.preventDefault();
+
+        axios.post('/users/1/subscriptions', this.state)
+            .then(res => {
+                this.setState({
+                    msg: res.data['msg']
+                })
+            })
+    };
+
+    render() {
+        return (
+            <Row>
+                <Col className="py-2">
+                    <Card>
+                        <Card.Body>
+                            <Form onSubmit={this.newSub}>
+                                <Form.Group>
+                                    <Form.Control type="text"
+                                                  value={this.state.show}
+                                                  onChange={this.changeShow}
+                                                  placeholder="Show name..."/>
+                                </Form.Group>
+                                <div className="text-center text-muted">
+                                    {this.state.msg}
+                                </div>
+                                <Button type="submit">
+                                    New Subscription
+                                </Button>
+                            </Form>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+        )
+    }
+}
