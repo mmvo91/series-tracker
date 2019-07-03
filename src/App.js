dc403ba9564a1116ffa62926c8ecc942653b4c5c
Michael Vo <michaelmvo91@gmail.com>
2019-07-03 10:47:10 -1000

    Adding staying logged in on refresh when fail

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import {library} from '@fortawesome/fontawesome-svg-core'
 import {faChevronRight, faTv} from '@fortawesome/free-solid-svg-icons'
 import Container from "react-bootstrap/Container";
 import {BrowserRouter as Router, Route} from "react-router-dom";
+import api from "./Api"
 import PrivateRoute from "./components/PrivateRoute"
 import Navigation from "./components/Navigation";
+import Spinner from "react-bootstrap/Spinner";
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login"
 import Register from "./pages/Register";
@@ -17,27 +19,57 @@ import Queue from "./pages/Queue";
 import New from "./pages/New"
 import Upcoming from "./pages/Upcoming";
 
+import UserStore from "./stores/UserStore"
+import {connect} from "overstated"
+
 library.add(faChevronRight, faTv);
 
-function App() {
-    return (
-        <Router>
-            <Navigation/>
-            <Container fluid className="py-2">
-                <PrivateRoute exact path="/" component={Dashboard}/>
-                <Route path="/login" component={Login}/>
-                <Route path="/register" component={Register}/>
-                <Route path="/reset" component={Reset}/>
-                <PrivateRoute exact path="/new" component={New}/>
-                <PrivateRoute exact path="/upcoming" component={Upcoming}/>
-                <PrivateRoute exact path="/queue" component={Queue}/>
-                <PrivateRoute exact path="/shows" component={Shows}/>
-                <PrivateRoute exact path="/shows/:id" component={Episodes}/>
-                <PrivateRoute exact path="/shows/:id/season" component={Seasons}/>
-                <PrivateRoute exact path="/shows/:id/season/:season" component={Episodes}/>
-            </Container>
-        </Router>
-    );
+class App extends React.Component {
+    state = {
+        auth: null
+    };
+
+    componentWillMount = () => {
+        api.get('/token')
+            .then(res => {
+                this.props.store.loggedIn(res.data['id']);
+                this.setState({auth: true})
+            })
+            .catch(res => {
+                this.setState({auth: false})
+            })
+    };
+
+
+    render() {
+        if (this.state.auth !== null) {
+            return (
+                <Router>
+                    <Navigation/>
+                    <Container fluid className="py-2">
+                        <PrivateRoute exact path="/" component={Dashboard}/>
+                        <Route path="/login" component={Login}/>
+                        <Route path="/register" component={Register}/>
+                        <Route path="/reset" component={Reset}/>
+                        <PrivateRoute exact path="/new" component={New}/>
+                        <PrivateRoute exact path="/upcoming" component={Upcoming}/>
+                        <PrivateRoute exact path="/queue" component={Queue}/>
+                        <PrivateRoute exact path="/shows" component={Shows}/>
+                        <PrivateRoute exact path="/shows/:id" component={Episodes}/>
+                        <PrivateRoute exact path="/shows/:id/season" component={Seasons}/>
+                        <PrivateRoute exact path="/shows/:id/season/:season" component={Episodes}/>
+                    </Container>
+                </Router>
+            );
+        } else {
+            return (
+                <div className="w-100 text-center py-2">
+                    <Spinner animation="border"
+                             variant="primary"/>
+                </div>
+            )
+        }
+    }
 }
 
-export default App;
+export default connect(UserStore)(App);
