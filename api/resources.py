b87f7bc56e3555fdaef5581764d5d2eb401ee274
Michael Vo <michaelmvo91@gmail.com>
2019-07-14 21:33:20 -1000

    Fix: Switch out access_token with refresh token check

--- api/resources.py
+++ api/resources.py
@@ -2,7 +2,7 @@ import datetime
 
 from flask import request, jsonify
 from flask_jwt_extended import (
-    create_access_token, create_refresh_token, set_access_cookies, set_refresh_cookies,
+    jwt_refresh_token_required, create_access_token, create_refresh_token, set_access_cookies, set_refresh_cookies,
     jwt_required, get_jwt_identity, unset_jwt_cookies
 )
 from flask_restful import Resource
@@ -18,8 +18,13 @@ TODAY = datetime.datetime.today()
 class Token(Resource):
     @jwt_required
     def get(self):
-        x = get_jwt_identity()
-        return {'id': x, 'msg': 'Valid token'}
+        current_user = get_jwt_identity()
+        access_token = create_access_token(identity=current_user)
+
+        response = {'id': current_user, 'msg': 'Valid token'}
+        set_access_cookies(response, access_token)
+
+        return response
 
     def post(self):
         data = request.json
