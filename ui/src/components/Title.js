963d875f45e0082200ec3c5bbb3dfe3b1ed41f2e
Michael Vo <michaelmvo91@gmail.com>
2020-05-08 13:00:53 -1000

    Tweaking title to allow for icons

--- ui/src/components/Title.js
+++ ui/src/components/Title.js
@@ -3,7 +3,10 @@ import React from "react";
 const Title = (props) => {
     return (
         <React.Fragment>
-            <h3 className="pt-3 m-0">{props.title}</h3>
+            <div className="pt-3">
+                <h3 className="m-0 d-inline">{props.title}</h3>
+                <span className="mx-2">{props.children}</span>
+            </div>
             <hr/>
         </React.Fragment>
     )
