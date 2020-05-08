f92f5c362838430ea0754a217c61c1944e072e8e
Michael Vo <michaelmvo91@gmail.com>
2020-05-08 09:56:26 -1000

    Adding ability to add New MovieGroup

--- ui/src/pages/MovieGroups.js
+++ ui/src/pages/MovieGroups.js
@@ -8,10 +8,66 @@ import Title from "../components/Title";
 
 import UserStore from "../stores/UserStore";
 import {useStore} from "overstated";
+import Card from "react-bootstrap/Card";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
+const NewMovieGroup = () => {
+    const {user_id} = useStore(UserStore, store => ({
+        user_id: store.state.id,
+    }));
+
+    const [msg, setMsg] = useState(null)
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        const data = {
+            name: e.target.name.value,
+            description: e.target.description.value,
+            type: e.target.type.value
+        }
+
+        api.post('users/' + user_id + '/movies/groups', data)
+            .then(res => (
+                setMsg(res.data['msg'])
+            ))
+    }
+
+    return (
+        <Card>
+            <Card.Body>
+                <Form onSubmit={handleSubmit}>
+                    <Form.Group controlId="name">
+                        <Form.Label>Name</Form.Label>
+                        <Form.Control/>
+                    </Form.Group>
+                    <Form.Group controlId="description">
+                        <Form.Label>Description</Form.Label>
+                        <Form.Control as="textarea" rows="3"/>
+                    </Form.Group>
+                    <Form.Group controlId="type">
+                        <Form.Label>Type</Form.Label>
+                        <Form.Control as="select">
+                            <option>Trilogy</option>
+                            <option>Universe</option>
+                            <option>Collection</option>
+                        </Form.Control>
+                    </Form.Group>
+                    <div className="text-center text-muted">
+                        {msg}
+                    </div>
+                    <Button type="submit">
+                        Add New Movie Group
+                    </Button>
+                </Form>
+            </Card.Body>
+        </Card>
+    )
+}
 
 const MovieGroups = () => {
-    const {user_id} = useStore (UserStore, store => ({
+    const {user_id} = useStore(UserStore, store => ({
         user_id: store.state.id,
     }));
 
@@ -23,7 +79,9 @@ const MovieGroups = () => {
     }, []);
 
     return (
-        <Container fluid className="py-2">
+        <Container fluid>
+            <Title title="Movie Groups"/>
+            <NewMovieGroup/>
             {
                 movieGroup !== null
                     ? (movieGroup.map(
