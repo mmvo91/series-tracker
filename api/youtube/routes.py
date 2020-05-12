56cb02eee00b40316dedf4efbf2f43f7b8c8a849
Michael Vo <michaelmvo91@gmail.com>
2020-05-11 20:28:53 -1000

    Adding YouTube routes

---
+++ api/youtube/routes.py
@@ -0,0 +1,13 @@
+from flask import Blueprint
+from flask_cors import CORS
+from flask_restful import Api
+
+from . import resources
+
+bp = Blueprint('youtube', __name__, url_prefix="/channels")
+youtube = Api(bp)
+CORS(bp, supports_credentials=True)
+
+youtube.add_resource(resources.Channels, "", strict_slashes=False)
+youtube.add_resource(resources.Channel, "/<channel_id>")
+youtube.add_resource(resources.Videos, "/<channel_id>/videos")
