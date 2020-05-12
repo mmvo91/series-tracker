1b635aa4e576771fe80a51d9b82f5fa58d475449
Michael Vo <michaelmvo91@gmail.com>
2020-05-11 20:27:19 -1000

    Adding YouTube schemas

---
+++ api/youtube/schemas.py
@@ -0,0 +1,51 @@
+from . import models
+from api.extensions import ma
+from api.utils.schema import CamelCaseSchema
+
+
+class VideoSchema(CamelCaseSchema):
+    class Meta:
+        model = models.Video
+        exclude = [
+            'ModifiedDate',
+            'ModifiedBy',
+            'CreatedDate',
+            'CreatedBy'
+        ]
+
+
+class ChannelSchema(CamelCaseSchema):
+    class Meta:
+        model = models.Channel
+        exclude = [
+            'ModifiedDate',
+            'ModifiedBy',
+            'CreatedDate',
+            'CreatedBy'
+        ]
+
+
+class SubscribedChannelSchema(CamelCaseSchema):
+    class Meta:
+        model = models.SubscribedChannel
+        exclude = [
+            'ModifiedDate',
+            'ModifiedBy',
+            'CreatedDate',
+            'CreatedBy'
+        ]
+
+    channel = ma.Nested(ChannelSchema)
+
+
+class SubscribedVideoSchema(CamelCaseSchema):
+    class Meta:
+        model = models.SubscribedVideo
+        exclude = [
+            'ModifiedDate',
+            'ModifiedBy',
+            'CreatedDate',
+            'CreatedBy'
+        ]
+
+    video = ma.Nested(VideoSchema)
