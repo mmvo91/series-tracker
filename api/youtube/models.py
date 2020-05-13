f26678bede1251e7adcf4c6e75f38f916c7f5d95
Michael Vo <michaelmvo91@gmail.com>
2020-05-12 21:11:17 -1000

    Moving SubscribedVideo above SubscribedChannel

--- api/youtube/models.py
+++ api/youtube/models.py
@@ -34,6 +34,22 @@ class Video(sql.Model):
     CreatedBy = sql.Column(sql.String, default="sqlAlchemy")
 
 
+class SubscribedVideo(sql.Model):
+    __tablename__ = 'subscribed_videos'
+
+    user_id = sql.Column(sql.Integer, sql.ForeignKey('users.id'), primary_key=True)
+    channel_id = sql.Column(sql.String, sql.ForeignKey('channels.id'), primary_key=True)
+    video_id = sql.Column(sql.String, sql.ForeignKey('videos.id'), primary_key=True)
+    watched = sql.Column(sql.Boolean, default=False)
+
+    video = sql.relationship('Video')
+
+    ModifiedDate = sql.Column(sql.DateTime, default=sql.func.now(), onupdate=sql.func.now())
+    ModifiedBy = sql.Column(sql.String, default="sqlAlchemy")
+    CreatedDate = sql.Column(sql.DateTime, default=sql.func.now())
+    CreatedBy = sql.Column(sql.String, default="sqlAlchemy")
+
+
 class SubscribedChannel(sql.Model):
     __tablename__ = "subscribed_channels"
 
@@ -46,22 +62,6 @@ class SubscribedChannel(sql.Model):
         primaryjoin="and_(SubscribedChannel.user_id==SubscribedVideo.user_id, SubscribedChannel.channel_id==SubscribedVideo.channel_id)",
         foreign_keys="[SubscribedVideo.user_id, SubscribedVideo.channel_id]"
     )
-
-    ModifiedDate = sql.Column(sql.DateTime, default=sql.func.now(), onupdate=sql.func.now())
-    ModifiedBy = sql.Column(sql.String, default="sqlAlchemy")
-    CreatedDate = sql.Column(sql.DateTime, default=sql.func.now())
-    CreatedBy = sql.Column(sql.String, default="sqlAlchemy")
-
-
-class SubscribedVideo(sql.Model):
-    __tablename__ = 'subscribed_videos'
-
-    user_id = sql.Column(sql.Integer, sql.ForeignKey('users.id'), primary_key=True)
-    channel_id = sql.Column(sql.String, sql.ForeignKey('channels.id'), primary_key=True)
-    video_id = sql.Column(sql.String, sql.ForeignKey('videos.id'), primary_key=True)
-    watched = sql.Column(sql.Boolean, default=False)
-
-    video = sql.relationship('Video')
 
     ModifiedDate = sql.Column(sql.DateTime, default=sql.func.now(), onupdate=sql.func.now())
     ModifiedBy = sql.Column(sql.String, default="sqlAlchemy")
