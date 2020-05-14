f693ab5c1b81cf17a7e80f8d1642b299d059a41b
Michael Vo <michaelmvo91@gmail.com>
2020-05-13 20:29:21 -1000

    Adding pagination to UI and API for Upcoming

--- api/shows/resources.py
+++ api/shows/resources.py
@@ -303,11 +303,11 @@ class Upcoming(Resource):
             models.Watched.episode.has(models.Episode.air_date > today()),
             models.Watched.user_id == get_jwt_identity(),
             models.Watched.watched.is_(False)
-        ).all()
+        )
 
-        schema = schemas.SubscriptionWatchSchema(many=True)
+        schema = schemas.SubscriptionWatchSchema()
 
-        return schema.dump(new)
+        return extensions.pagination.paginate(new, schema, True)
 
 
 class Recent(Resource):
--- ui/src/pages/Upcoming.js
+++ ui/src/pages/Upcoming.js
@@ -3,16 +3,20 @@ import Row from "react-bootstrap/Row";
 import Spinner from "react-bootstrap/Spinner"
 import api from "../Api"
 import SoloEpisode from "../components/SoloEpisodeCard";
+import Title from "../components/Title";
+import LoadMore from "../components/LoadMore";
 
 export default (class Upcoming extends Component {
     state = {
-        data: null
+        data: null,
+        show: false,
+        pagination: null,
     };
 
     componentDidMount() {
         api.get('/shows/upcoming')
             .then(res => {
-                this.setState({data: res.data});
+                this.setState({data: res.data['data'], pagination: res.data['pagination']});
             })
     }
 
@@ -48,31 +52,40 @@ export default (class Upcoming extends Component {
 
     render() {
         return (
-            <Row>
-                {
-                    this.state.data !== null
-                        ? (
-                            this.state.data.length !== 0
-                                ? (
-                                    this.state.data.map((datum) =>
-                                        (
-                                            <SoloEpisode
-                                                key={datum.episode.id}
-                                                update={this.update}
-                                                watched={datum.watched}
-                                                {...datum}/>
-                                        ))
-                                )
-                                : <div className="w-100 text-center py-2">Nothing Upcoming</div>
-                        )
-                        : (
-                            <div className="w-100 text-center py-2">
-                                <Spinner animation="border"
-                                         variant="primary"/>
-                            </div>
-                        )
-                }
-            </Row>
+            <div>
+                <Title title="Upcoming"/>
+                <Row>
+                    {
+                        this.state.data !== null
+                            ? (
+                                this.state.data.length !== 0
+                                    ? (
+                                        this.state.data.map((datum) =>
+                                            (
+                                                <SoloEpisode
+                                                    key={datum.episode.id}
+                                                    update={this.update}
+                                                    watched={datum.watched}
+                                                    {...datum}/>
+                                            ))
+                                    )
+                                    : <div className="w-100 text-center py-2">Nothing Upcoming</div>
+                            )
+                            : (
+                                <div className="w-100 text-center py-2">
+                                    <Spinner animation="border"
+                                             variant="primary"/>
+                                </div>
+                            )
+                    }
+                </Row>
+                <LoadMore
+                    pagination={this.state.pagination}
+                    newPagination={(e) => this.setState({pagination: e})}
+                    data={this.state.data}
+                    newData={(e) => this.setState({data: e})}
+                />
+            </div>
         )
     }
 })
