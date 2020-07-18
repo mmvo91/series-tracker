a96f531cac6152ef4276473bccd2c2c8c45c12fa
Michael Vo <michaelmvo91@gmail.com>
2020-07-18 09:47:05 -1000

    Adding update to SoloEpisode component

--- ui/src/components/SoloEpisodeCard.js
+++ ui/src/components/SoloEpisodeCard.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, {useState} from "react";
+import api from "../Api"
 import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
 import Image from "react-bootstrap/Image";
@@ -6,6 +7,23 @@ import moment from "moment-timezone"
 import IconButton from "../components/IconButton"
 
 const SoloEpisode = (props) => {
+    const [watched, setWatched] = useState(props.watched)
+
+    const update = (e) => {
+        e.preventDefault();
+
+        const data = {
+            id: e.target.id,
+            watched: !watched
+        }
+
+        api.put('/shows/subscriptions/' + e.target.value + '/episodes', data)
+            .then(() => {
+                setWatched(!watched)
+            })
+
+    }
+
     return (
         <Col md={4} lg={3} className="py-2">
             <Card className="h-100">
@@ -13,23 +31,30 @@ const SoloEpisode = (props) => {
                     <Card.Title>{props.episode.name}</Card.Title>
                     <Card.Subtitle>{props.show.name}</Card.Subtitle>
                     <Image fluid src={props.episode.image} className="mx-auto d-block my-3"/>
-                    <p><b>Air Date: </b>{moment.tz(props.episode.air_date, 'Etc/UTC').tz(moment.tz.guess(true)).format('MMMM Do YYYY @ hh:mm A z')}</p>
+                    <p>
+                        <b>Air Date: </b>
+                        {
+                            moment.tz(props.episode.air_date, 'Etc/UTC')
+                                .tz(moment.tz.guess(true))
+                                .format('MMMM Do YYYY @ hh:mm A z')
+                        }
+                    </p>
                     <p><b>Season: </b>{props.episode.season} <b>Episode: </b>{props.episode.number}</p>
                     <div dangerouslySetInnerHTML={{__html: props.episode.summary}}/>
                 </Card.Body>
                 <Card.Footer>
                     <div className="float-right">
                         {
-                            props.watched
+                            watched
                                 ? <IconButton
-                                    update={props.update}
+                                    update={update}
                                     id={props.episode.id}
                                     value={props.episode.show_id}
                                     icon="check"
                                     color="text-success"
                                     text="Watched"/>
                                 : <IconButton
-                                    update={props.update}
+                                    update={update}
                                     id={props.episode.id}
                                     value={props.episode.show_id}
                                     icon="check"
